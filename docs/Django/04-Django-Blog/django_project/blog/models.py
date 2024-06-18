fd5cb52ec0690ddda8d83af27c07422c3c3e1728
EricYang1207 <yalin.yang@dallas.gov>
2024-06-18 16:32:27 -0500

    finish ch2 in django

--- docs/Django/04-Django-Blog/django_project/blog/models.py
+++ docs/Django/04-Django-Blog/django_project/blog/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=50)
-    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=80)
+    author = models.ForeignKey(User, on_delete= models.CASCADE)
     content = models.TextField()
-    date_posted = models.DateTimeField(auto_now_add=True)
+    date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
