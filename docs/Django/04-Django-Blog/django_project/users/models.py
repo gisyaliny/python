5a7ab6f616984f4969faf05b3c4ca652853af87a
gisyaliny <690521906@qq.com>
2024-07-01 15:15:55 -0400

    update

--- docs/Django/04-Django-Blog/django_project/users/models.py
+++ docs/Django/04-Django-Blog/django_project/users/models.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField()
+    bio = models.TextField(default="This user do not have a bio",blank=True)
+    image = models.ImageField(default='default.png',upload_to='profile_pics')
     birth_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return f'{self.user.username} Profile'
+ 
